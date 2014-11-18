178d4b6eeec7f60066fbca24c0172d0ad1812e5b
dcuddhy <dcuddhy@gmail.com>
2014-11-18 11:22:29 -0700

    done-zo

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,5 @@
 class Organization < ActiveRecord::Base
+  has_many :employments # <= there's a post_id column on the taggings table
+  has_many :people, through: :employments   # <= there's a tag_id column on the taggings table
+
 end
